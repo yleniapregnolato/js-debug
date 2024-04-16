acb694c406e5d2188efa58e143cd6f7008796669
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-16 18:47:21 +0200

    feat: bundle-02 esercizio 1

--- debug-excercises/js-debug/bundle-02/main.js
+++ debug-excercises/js-debug/bundle-02/main.js
@@ -23,71 +23,76 @@ function checkAge() {
 }
 checkAge();
 
+// 1- questa funzione ci dice se abbiamo più o meno di 18
+// 2- non sono presenti errori di sintassi
+// 3- sono presenti errori logici in quanto si è dato un valore fisso alla costante myAge e in questo caso il risultato sarà sempre e comunque >18. sarebbe stato più corretto chiedere l'età tramite un prompt dentro un parseint: const myAGe = parseInt(prompt("quanti anni hai?"));
+// l'altro errore è nella costante message, il suo valore cambia nel corso dell'esecuzione del codice quindi bisogna dichiararla con un let
+
 // ESERCIZIO 2
-function printColorsNumber() {
-    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
-}
-printColorsNumber();
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
 
 
 // ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
-
-
-// ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
-
-    let grantAccess = 'false';
-
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
-
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
 
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
-function checkAccessImproved() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+// // ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+//     let grantAccess = 'false';
 
-    let grantAccess = 'false';
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
 
-    for (let i = 0; i < addresses.length; i++) {
-        const email = addresses[i];
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
 
-        if (userEmail.length > 5) {
 
-            if (email === userEmail) {
-                grantAccess = 'true';
+// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// function checkAccessImproved() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
-            }
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-        }
+//     let grantAccess = 'false';
 
-        if (grantAccess) {
-            console.log('Accesso consentito!');
-        } else {
-            console.log('Accesso negato!');
-        }
-    }
-    checkAccessImproved();
+//     for (let i = 0; i < addresses.length; i++) {
+//         const email = addresses[i];
+
+//         if (userEmail.length > 5) {
+
+//             if (email === userEmail) {
+//                 grantAccess = 'true';
+
+//             }
+
+//         }
+
+//         if (grantAccess) {
+//             console.log('Accesso consentito!');
+//         } else {
+//             console.log('Accesso negato!');
+//         }
+//     }
+//     checkAccessImproved();
 
 
 
