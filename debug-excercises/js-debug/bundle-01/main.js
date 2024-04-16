245573ad96dd06ea377d5395a972a4c2c929ddf6
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-16 16:08:15 +0200

    fix: bundle-01 esercizio 1

--- debug-excercises/js-debug/bundle-01/main.js
+++ debug-excercises/js-debug/bundle-01/main.js
@@ -14,17 +14,17 @@
 // for (let i = 0; i > 5; i++) {
 //     console.log(i);
 // }
-// 1- è un ciclo for che incrementa l'indice quando è maggiore di 5
+// 1- è un ciclo for che stampa numeri quando sono maggiori di 5
 // 2- non sono presenti errori di sintassi
 // 3- l'errore logico è il ">" in quanto "i" parte da 0 mentre la condizione del ciclo è che "i" sia maggiore di 5, quindi in questi termini il ciclo non verrà mai avviato. Il codice corretto sarebbe (let i = 0; i < 5; i++)
 
 // ESERCIZIO 2
-function addIfEven(num) {
-    if (num % 2 = 0) {
-        return num + 5;
-    }
-    return num;
-}
+// function addIfEven(num) {
+//     if (num % 2 = 0) {
+//         return num + 5;
+//     }
+//     return num;
+// }
 
 // 1- è una funzione che ci dice se il numero inserito è pari
 // 2- sono presenti errori di sintassi in quanto scrivere "=" significa fare un'assegnazione quindi stiamo dicendo che il num % 2 è uguale a 0. Noi invece abbiamo bisgno di un operatore di confronto che sia strettamente uguale, quindi di un'indentità. il codice corretto quindi è if (num% 2 === 0)
@@ -32,11 +32,14 @@ function addIfEven(num) {
 
 
 // ESERCIZIO 3
-// function loopToFive() {
-//     for (let i = 0, i < 5, i++) {
-//         console.log(i);
-//     }
-// }
+function loopToFive() {
+    for (let i = 0, i < 5, i++) {
+        console.log(i);
+    }
+}
+
+// 1- è una funzione che ci da un ciclo for per stampare numeri inferiori di 5
+
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
