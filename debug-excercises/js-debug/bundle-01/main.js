e4d1aaebd69e8beb93f4b381250e0e5983714085
yleniapregnolato <yleniapregnolato@gmail.com>
2024-04-16 16:52:48 +0200

    fix: bundle-01 esercizio 2

--- debug-excercises/js-debug/bundle-01/main.js
+++ debug-excercises/js-debug/bundle-01/main.js
@@ -11,22 +11,22 @@
 
 
 // ESERCIZIO 1
-// for (let i = 0; i > 5; i++) {
-//     console.log(i);
-// }
+for (let i = 0; i > 5; i++) {
+    console.log(i);
+}
 // 1- è un ciclo for che stampa numeri quando sono maggiori di 5
 // 2- non sono presenti errori di sintassi
 // 3- l'errore logico è il ">" in quanto "i" parte da 0 mentre la condizione del ciclo è che "i" sia maggiore di 5, quindi in questi termini il ciclo non verrà mai avviato. Il codice corretto sarebbe (let i = 0; i < 5; i++)
 
 // ESERCIZIO 2
-// function addIfEven(num) {
-//     if (num % 2 = 0) {
-//         return num + 5;
-//     }
-//     return num;
-// }
+function addIfEven(num) {
+    if (num % 2 = 0) {
+        return num + 5;
+    }
+    return num;
+}
 
-// 1- è una funzione che ci dice se il numero inserito è pari
+// 1- è una funzione che ci dice se il numero inserito è pari, in quel caso verrà sommato a 5 e ci restituirà il risultato
 // 2- sono presenti errori di sintassi in quanto scrivere "=" significa fare un'assegnazione quindi stiamo dicendo che il num % 2 è uguale a 0. Noi invece abbiamo bisgno di un operatore di confronto che sia strettamente uguale, quindi di un'indentità. il codice corretto quindi è if (num% 2 === 0)
 // 3- non sono presenti errori logici
 
@@ -45,14 +45,14 @@ function loopToFive() {
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
-// function displayEvenNumbers() {
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i = 0; i < numbers.length - 1; i++;) {
-//         if (numbers % 2 = 0); {
-//             evenNumbers.push(i);
-//         }
-//         return evenNumbers;
-//     }
-// }
-// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length - 1; i++;) {
+        if (numbers % 2 = 0); {
+            evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
+displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
